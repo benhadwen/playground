df4ac1384f683fbcca0a72da13eac03dd206cfd6
Ben Hadwen <hadwenb@frenchconnection.com>
2014-03-07 18:10:21 +0000

    slideshow in progress

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	var numslides = 4;
 	$('a.prev').click(function(){
 		console.log("prev");
+		$('.slideshow ul').css("left", "-25%");
 	});
 	$('a.next').click(function(){
 		console.log("next");
