4489d330129f926cad027807d38642ce182961b2
Ben Hadwen <hadwenb@frenchconnection.com>
2014-03-07 17:43:13 +0000

    start building slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,10 @@
+// main js
+$(document).ready(function(){
+	var numslides = 4;
+	$('a.prev').click(function(){
+		console.log("prev");
+	});
+	$('a.next').click(function(){
+		console.log("next");
+	});
+});
