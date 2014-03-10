27312abc2a1dd863e4bc0eb23dbad9be95d102be
Ben Hadwen <hadwenb@frenchconnection.com>
2014-03-10 17:39:06 +0000

    refactor code and comment

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,87 +1,75 @@
 // main js
 $(document).ready(function(){
+	// INITIALIZE
 	// get number of slides
-	// var numslides = $('.slideshow ul').children('li').length;
-	var numSlides = $('.slideshow>ul>li').length;
-	var currentSlideNum = 1;
-	var currentSlide = $('.slideshow>ul>li:nth-child('+ currentSlideNum +')').addClass('active');
-	var nextSlide;
+	var numSlides = $('.slideshow>ul>li').length; 	// calculate lenght based on length of slide list
+	var currentSlideNum = 1;  						// set starting slide here
+	var nextSlideNum;                               // initialize next slide num to 
 
-	console.log("numslides: " + numSlides);
-	console.log("currentSlideNum: " + currentSlideNum);
+	// set the current slide to be active which raises to z-index for visibility in the css
+	$('.slideshow>ul>li:nth-child('+ currentSlideNum +')').addClass('active');
+
+	// ANIMATION FUNCTION
+	function slideAnimate(currSlideNum, nxtSlideNum, direction){
+		// get the current and next slides in the DOM
+		var currSlide = $('.slideshow>ul>li:nth-child('+ currSlideNum +')');
+		var nxtSlide = $('.slideshow>ul>li:nth-child('+ nxtSlideNum +')').addClass('active');
+		
+		if(direction === "next"){
+			$(nxtSlide).css("left", "100%"); // move the next slide off screen to the right
+			$(currSlide).animate({
+				left: "-100%"},              // animate current slide off scrrent to the left
+				"slow", function() {
+					$(currSlide).removeClass('active'); 
+			});
+		}
+
+		if(direction === "prev"){
+			$(nxtSlide).css("left", "-100%");
+			$(currSlide).animate({
+				left: "100%"},
+				"slow", function() {
+					$(currSlide).removeClass('active');
+			});
+		}
+
+		$(nxtSlide).animate({left: 0}, "slow");  // animate next slide on screen from the right or left depending on direction
+	}
+
+	// PREV BUTTON
 	$('a.prev').click(function(){
-		console.log("prev");
-		var nextSlideNum;
-		console.log("currentSlideNum: " + currentSlideNum);
+		var direction = "prev";
 		if((currentSlideNum - 1) == 0)
 		{
-			console.log("move back to slide end of slide show [numSlides]");
+			// move to last slide if current slide is first slide
 			nextSlideNum = numSlides;
-			//do animation here
+			slideAnimate(currentSlideNum, nextSlideNum, direction);
 			currentSlideNum = numSlides;
-			console.log("currentSlideNum: " + currentSlideNum);
 		}
 		else{
-			console.log("move to next slide");
+			// go to prev slide
 			nextSlideNum = currentSlideNum - 1;
-			//do animation here
+			slideAnimate(currentSlideNum, nextSlideNum, direction);
 			currentSlideNum--;
-			console.log("currentSlideNum: " + currentSlideNum);
 		}
 	});
 
+
+	// NEXT BUTTON
 	$('a.next').click(function(){
-		var nextSlideNum;
-		console.log("next");
+		var direction = "next";
 		if((currentSlideNum + 1) > numSlides)
 		{
-			console.log("move back to slide 1");
+			// move back to slide 1 if at end of slideshow
 			nextSlideNum = 1;
-			nextSlide = $('.slideshow>ul>li:nth-child('+ nextSlideNum +')').addClass('active');
-			//do animation here
-			$(nextSlide).css("left", "100%");
-			$(currentSlide).animate({
-				"left": 0,
-				"left": "-100%"},
-				"slow", function() {
-					$(currentSlide).removeClass('active');
-					currentSlide = nextSlide;
-				/* stuff to do after animation is complete */
-			});
-
-			$(nextSlide).animate({
-				"left": "100%",
-				"left": 0},
-				"slow", function() {
-				/* stuff to do after animation is complete */
-			});
-			//end animation
+			slideAnimate(currentSlideNum, nextSlideNum, direction);
 			currentSlideNum = 1;
-			console.log("currentSlideNum: " + currentSlideNum);
 		}
 		else{
-			nextSlideNum = currentSlideNum + 1;
-			nextSlide = $('.slideshow>ul>li:nth-child('+ nextSlideNum +')').addClass('active');
-			//do animation here
-			$(nextSlide).css("left", "100%");
-			$(currentSlide).animate({
-				"left": 0,
-				"left": "-100%"},
-				"slow", function() {
-					$(currentSlide).removeClass('active');
-					currentSlide = nextSlide;
-				/* stuff to do after animation is complete */
-			});
 
-			$(nextSlide).animate({
-				"left": "100%",
-				"left": 0},
-				"slow", function() {
-				/* stuff to do after animation is complete */
-			});
-			//end animation
+			nextSlideNum = currentSlideNum + 1;
+			slideAnimate(currentSlideNum, nextSlideNum, direction);
 			currentSlideNum++;
-			console.log("currentSlideNum: " + currentSlideNum);
 		}
 	});
 });
